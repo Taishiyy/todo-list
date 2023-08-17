d90533850af4803591afcb064046c8243db8ddb4
Taishiyy <taishi.y.mj@gmail.com>
2023-08-17 23:23:33 +0900

    some work done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-/**
- * todo-list
- */
+// import "./styles.css";
+
+const onClickAdd = () => {
+    //textboxの値を取得し、初期化する
+    const inputText = document.getElementById("add-text").value;
+    document.getElementById("add-text").value = "";
+
+    //div生成
+    const div = document.createElement("div");
+    div.className = "list-row";
+
+    //p生成
+    const p = document.createElement("p");
+    p.className = "one-list";
+    p.innerText = inputText;
+
+    //divタグの子要素に各要素を設定
+    div.appendChild(p);
+    console.log(div);
+
+    //未完了のリストに追加
+    document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+    .getElementById("add-button")
+    .addEventListener("click", () => onClickAdd());
